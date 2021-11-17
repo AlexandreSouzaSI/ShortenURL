fcb5a68e5df723bd5f613cd70bd689a103ee5e4f
Alexandre Moura <alemoura23@hotmail.com>
2021-11-17 01:24:54 -0300

    ProjetoShortenURL

---
+++ dist/config/Constants.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.config = void 0;
+exports.config = {
+    API_URL: 'http://localhost:3000',
+    MONGO_CONNECTION: 'mongodb+srv://alemoura:92988096@encurtador-url.jk0ob.mongodb.net/encurtador-url?retryWrites=true&w=majority'
+};
+//# sourceMappingURL=Constants.js.map
---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+    API_URL: 'http://localhost:3000',
+    MONGO_CONNECTION:
+    'mongodb+srv://alemoura:92988096@encurtador-url.jk0ob.mongodb.net/encurtador-url?retryWrites=true&w=majority'
+}
